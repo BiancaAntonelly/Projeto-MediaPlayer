41070731bbde4662a71527b8f39e7cd8c6a9ac96
Vanessa Maria <vanessa.silva5205@gmail.com>
2023-11-23 00:19:56 -0300

    Criada a classe música e feita a implementação de PlayList

--- src/br/ufrn/imd/model/Playlist.java
+++ src/br/ufrn/imd/model/Playlist.java
@@ -1,4 +1,28 @@
 package br.ufrn.imd.model;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Playlist {
+    private String name, categoria;
+    private List<Musica> musicas = new ArrayList<>();
+
+    public String getName() {
+        return name;
+    }
+    public void setName(String name) {
+        this.name = name;
+    }
+    public String getCategoria() {
+        return categoria;
+    }
+    public void setCategoria(String categoria) {
+        this.categoria = categoria;
+    }
+    public List<Musica> getMusicas() {
+        return musicas;
+    }
+    public void setMusicas(List<Musica> musicas) {
+        this.musicas = musicas;
+    }
 }
