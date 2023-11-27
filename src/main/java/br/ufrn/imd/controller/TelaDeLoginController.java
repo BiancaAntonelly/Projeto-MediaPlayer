0bdc24a9bca65c53f540440de384876239b84acf
BIANCA ANTONELLY MEDEIROS DOS SANTOS <100948110+BiancaAntonelly@users.noreply.github.com>
2023-11-27 20:35:23 -0300

    implementando a tela de login

---
+++ src/main/java/br/ufrn/imd/controller/TelaDeLoginController.java
@@ -0,0 +1,24 @@
+package br.ufrn.imd.controller;
+
+import javafx.fxml.FXML;
+
+import java.awt.*;
+
+public class TelaDeLoginController {
+        @FXML
+        private Button btnEntrar;
+
+        @FXML
+        private Label lbEmail;
+
+        @FXML
+        private Label lbSenha;
+
+        @FXML
+        private TextField tfEmail;
+
+        @FXML
+        private TextField tfSenha;
+
+
+}
